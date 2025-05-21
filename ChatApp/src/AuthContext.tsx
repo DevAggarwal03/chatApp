c617af96f3a573c84ed775f87db4ed2b96e4012e
Dev Aggarwal <devaggarwal1009@gmail.com>
2025-05-21 16:23:19 +0530

    build error

--- ChatApp/src/AuthContext.tsx
+++ ChatApp/src/AuthContext.tsx
@@ -1,7 +1,6 @@
 import axios from "axios";
 import React, { createContext, useContext } from "react";
 import { useState } from "react";
-import { SocketContext } from "./AppContext";
 export const AuthContext = createContext<any>('');
 
 
